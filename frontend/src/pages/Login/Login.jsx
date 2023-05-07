25360d6d084e4c2acfa582b1d812852551e841f6
Protyush Mukherjee <protyush@gmail.com>
2023-05-07 13:32:11 +0530

    QUIZ-4

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+//import { LogoutOutlined } from "@ant-design/icons";
+import { Row, Col, Menu, Image } from "antd";
+import "./Login.css";
+import loginpic from "../../assests/loginpic2.png";
 
 const Login = () => {
   return (
-    <>
-      <h1>Login Pls</h1>
-    </>
+    <Row style={{ height: "90vh" }}>
+      <Col span={12} className="loginpic">
+        {/* <Image src={loginpic} className="loginpic"/>  */}
+      </Col>
+      <Col span={12}>
+
+      </Col>
+    </Row>
   );
 };
 
