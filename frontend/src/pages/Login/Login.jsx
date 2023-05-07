0a06f39a1b37fa2fa400c2c017aa3f7bf039ed46
Akash Ghosh <akashleo2009@gmail.com>
2023-05-07 14:00:06 +0530

    QUIZ-4, Dashboard update

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 //import { LogoutOutlined } from "@ant-design/icons";
-import { Row, Col, Menu, Image } from "antd";
-import "./Login.css";
-import loginpic from "../../assests/loginpic2.png";
 
-import { Row, Col, Form, Input, Button } from "antd";
 import {
   LeftOutlined,
   EyeTwoTone,
@@ -12,15 +8,8 @@ import {
   GoogleOutlined,
 } from "@ant-design/icons";
 
-import "./Login.css";
 
 import { Row, Col, Form, Input, Button } from "antd";
-import {
-  LeftOutlined,
-  EyeTwoTone,
-  EyeInvisibleOutlined,
-  GoogleOutlined,
-} from "@ant-design/icons";
 
 import "./Login.css";
 
