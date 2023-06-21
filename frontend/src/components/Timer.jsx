50779439afbeba349a4bba026f25ca14e3fba395
Akash Ghosh <akashleo2009@gmail.com>
2023-06-21 23:59:06 +0530

    QUIZ-36, timer component added

---
+++ frontend/src/components/Timer.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(prevSeconds => prevSeconds + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Timer: {seconds}s</h1>
+    </div>
+  );
+};
+
+export default Timer;
