d09f2d602467df6a0dbfb69416e9013305f97048
Akash Ghosh <akashleo2009@gmail.com>
2023-06-25 22:04:37 +0530

    QUIZ-51, GCP cloud storage integration done

--- backend/routes/answerRoutes.js
+++ backend/routes/answerRoutes.js
@@ -7,9 +7,6 @@ import {
   deleteAnswer,
 } from "../controllers/AnswerController.js";
 
-//const Book = require("../model/Profile");
-
-//const ProfileControllers = require("../controllers/ProfileController");
 
 answerRouter.get("/", getAllAnswers);
 //answerRouter.get("/:id", getProfileById);
@@ -17,6 +14,5 @@ answerRouter.post("/", addAnswer);
 answerRouter.put("/:id", updateAnswer);
 answerRouter.delete("/:id", deleteAnswer);
 
-//module.exports = answerRouter;
 
 export default answerRouter;
